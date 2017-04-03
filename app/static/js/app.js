1e5c3f45554ee9e47e0d022b9df3d6fd28ab39eb
aces774 <krystan09@yahoo.com>
2017-04-03 13:56:28 -0500

    Fianl Push

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,4 @@
-// Your JavaScript Code here
-// Your JavaScript Code here
+
 var app = angular.module('MyApp', []);
 app.controller('MainController', function($scope, $http){
 	$http.get("/api/thumbnails").then(function(response){
